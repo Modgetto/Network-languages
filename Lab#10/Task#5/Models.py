7840a3cb0a95fc7c60e795951b8ed7940a28d93d
Modgetto <114920465+Modgetto@users.noreply.github.com>
2024-06-25 11:30:15 +0300

    Delete Lab#10 directory

--- Lab#10/Task#5/Models.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
-
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(200), nullable=False)
-    content = db.Column(db.Text, nullable=False)
